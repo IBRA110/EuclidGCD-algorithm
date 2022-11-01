548f80dba43503d704ac41edcec73fed6c8f16c7
Ibrahim <mamedovarbeiter@gmail.com>
2022-11-01 11:52:18 +0300

    algorithm complete

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-﻿
-namespace Program
+﻿namespace Program
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string[] numbers = Console.ReadLine().Split(' ');
+            long a = Convert.ToInt64(numbers[0]);
+            long b = Convert.ToInt64(numbers[1]);
+
+            Program P = new Program();
+            Console.WriteLine(P.EuclidGCD(a, b));
+        }
+
+        public long EuclidGCD(long a, long b)
+        {
+            long c = 0;
+            while (b != 0)
+            {
+                c = a % b;
+                a = b;
+                b = c;
+            }
+            return a;
         }
     }
 }
